7cad5ce4abd08c87e596e3357176691a698c0ba4
José Guilherme Sório <josesorio@prefeitura.sp.gov.br>
2024-04-18 15:44:40 -0300

    feat: adjusts memory and change api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'https://evernote-api-production-624e1b1bd0b1.herokuapp.com//api',
+    baseURL: 'https://evernote-api-production-624e1b1bd0b1.herokuapp.com/api',
 });
 
 export default Api;
